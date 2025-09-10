284cec1a619ef7991a6c23b7f7dcb1ccad880a3d
foo <humai.foo@gmail.com>
2025-09-11 03:59:14 +0800

    disable swagger

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddScoped<IPinService, PinService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || true)
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
